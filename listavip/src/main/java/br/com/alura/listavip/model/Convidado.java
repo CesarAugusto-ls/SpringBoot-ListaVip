e8781e87e9ab478adfd2b7e0e78fc3b511307242
ITI-DEVS <ITI-DEVS@10.0.2.15>
2018-08-06 17:43:27 -0300

    4 > Criando CRUD

--- listavip/src/main/java/br/com/alura/listavip/model/Convidado.java
+++ listavip/src/main/java/br/com/alura/listavip/model/Convidado.java
@@ -14,6 +14,17 @@ public class Convidado {
 	private String email;
 	private String telefone;
 	
+	public Convidado() {
+		
+	}
+	
+	public Convidado(String nome, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+	
 	public long getId() {
 		return id;
 	}
